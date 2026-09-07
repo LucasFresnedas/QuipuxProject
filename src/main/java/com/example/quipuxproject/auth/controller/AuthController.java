8d3b391cb7425ed2edad9a8984f4556b5d4436f3
Lucas Fresnedas <92407228+LucasFresnedas@users.noreply.github.com>
2026-09-07 11:48:36 -0300

    Add more logs

--- src/main/java/com/example/quipuxproject/auth/controller/AuthController.java
+++ src/main/java/com/example/quipuxproject/auth/controller/AuthController.java
@@ -6,12 +6,14 @@ import com.example.quipuxproject.auth.dto.response.LoginResponse;
 import com.example.quipuxproject.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
@@ -24,6 +26,7 @@ public class AuthController {
     @PostMapping
     public ResponseEntity<LoginResponse> autentificacao(
             @RequestBody @Valid LoginRequest loginRequest){
+        log.info("Requisição recebida na camada de autentificação");
         return ResponseEntity.ok(authService.autenticar(loginRequest));
     }
 }
